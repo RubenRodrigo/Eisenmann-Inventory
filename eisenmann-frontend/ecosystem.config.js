3e453d4ee6ad28f2cfd5eac7c303270762836f3b
Ruben Rodrigo Chañi Laura <ruben.chani@tecsup.edu.pe>
2021-10-02 10:28:01 -0500

    Docker config

---
+++ eisenmann-frontend/ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	apps: [
+		{
+			name: 'eisenmann',
+			cwd: '/home/tkmiz/Documents/Projects/Eisenemann/eisenmann-inventory',
+			script: 'npm',
+			args: 'start',
+		},
+		// optionally a second project
+	],
+};
