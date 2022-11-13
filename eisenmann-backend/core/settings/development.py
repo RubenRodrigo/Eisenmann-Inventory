84eeb3b4153654c843d2563492808db0035c72b0
Ruben Rodrigo <rodrigohde905@gmail.com>
2022-11-13 17:59:30 -0500

    chore: remove entrypoint and add env variables

--- eisenmann-backend/core/settings/development.py
+++ eisenmann-backend/core/settings/development.py
@@ -1,7 +1,5 @@
 from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
-print(os.environ.get("DEBUG"))
-print(int(os.environ.get("DEBUG", default=0)))
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
